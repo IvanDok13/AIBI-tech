eb91e17dcc1997ab9e012e157c772cd6b07eda64
Ivan Klochkov <vany-nov13@yandex.ru>
2025-08-30 15:00:08 -0700

    feat: update site with slider

--- MvcMovie/wwwroot/js/site.js
+++ MvcMovie/wwwroot/js/site.js
@@ -1,4 +1,5 @@
 ﻿import { movies } from './data/data.js';
+import { initSlider, nextSlide, prevSlide } from './slider/slider.js';
 
 export function changeBackground() {
   const randomIndex = Math.floor(Math.random() * movies.length);
@@ -13,4 +14,16 @@ export function changeBackground() {
   document.getElementById('movie-description').textContent = movie.description;
 }
 
-document.addEventListener('DOMContentLoaded', changeBackground);
+document.addEventListener('DOMContentLoaded', () => {
+  changeBackground();
+
+  const slider = document.getElementById('slider');
+  const indicatorsContainer = document.getElementById('indicators');
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
+
+  initSlider(slider, indicatorsContainer, movies);
+
+  prevBtn.addEventListener('click', prevSlide);
+  nextBtn.addEventListener('click', nextSlide);
+});
