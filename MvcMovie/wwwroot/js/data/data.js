2aa005893c1e39752ddd914e266ed3656e3318b8
Ivan Klochkov <vany-nov13@yandex.ru>
2025-08-30 13:58:48 -0700

    feat: add movie data

---
+++ MvcMovie/wwwroot/js/data/data.js
@@ -0,0 +1,47 @@
+export const movies = [
+  {
+    title: 'The Dark Knight',
+    year: 2008,
+    rating: 9.0,
+    description:
+      'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+    posterPath:
+      'https://image.tmdb.org/t/p/original/qJ2tW6WMUDux911r6m7haRef0WH.jpg',
+  },
+  {
+    title: 'Inception',
+    year: 2010,
+    rating: 8.8,
+    description:
+      'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.',
+    posterPath:
+      'https://image.tmdb.org/t/p/original/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg',
+  },
+  {
+    title: 'Pulp Fiction',
+    year: 1994,
+    rating: 8.9,
+    description:
+      'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+    posterPath:
+      'https://image.tmdb.org/t/p/original/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg',
+  },
+  {
+    title: 'The Shawshank Redemption',
+    year: 1994,
+    rating: 9.3,
+    description:
+      'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+    posterPath:
+      'https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
+  },
+  {
+    title: 'Forrest Gump',
+    year: 1994,
+    rating: 8.8,
+    description:
+      'The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate, and other historical events unfold through the perspective of an Alabama man with an IQ of 75.',
+    posterPath:
+      'https://image.tmdb.org/t/p/original/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg',
+  },
+];
